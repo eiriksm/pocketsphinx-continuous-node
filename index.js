f1bb3c607ecfd4632dc5a63b25ea4c69272f71cf
Eirik S. Morland <eirik@morland.no>
2016-01-20 18:17:28 +0100

    Refactor to emitters.

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 'use strict';
 
 var spawn = require('child_process').spawn;
-module.exports = function(config, callback) {
-  var setId = config.setId;
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+function PocketSphinxContinuous(config) {
+  this.setId = config.setId;
+  this.verbose = config.verbose;
+  EventEmitter.call(this);
+
   var pc = spawn('pocketsphinx_continuous', [
     '-inmic',
     'yes',
     '-lm',
-    setId + '.lm',
+    this.setId + '.lm',
     '-dict',
-    setId + '.dic'
+    this.setId + '.dic'
   ]);
+
+  var psc = this;
+
   pc.stdout.on('data', function(data) {
     var output = data.toString().trim();
     if (output) {
-      callback(null, output);
+      psc.emit('data', output);
     }
   });
   pc.stderr.on('data', function(data) {
     var output = data.toString().trim();
     if (config.verbose && output) {
-      callback(null, data);
+      psc.emit('debug', data);
     }
   });
   pc.on('close', function(code) {
-    callback(code);
+    psc.emit('error', code);
   });
-};
+  pc.on('error', function(err) {
+    psc.emit('error', err);
+  })
+}
+
+util.inherits(PocketSphinxContinuous, EventEmitter);
+
+module.exports = PocketSphinxContinuous;
